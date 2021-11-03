68d0dbfc1790eb3c22974cc75a678934b34bbfee
CodingEinhornDeu <88826323+CodingEinhornDeu@users.noreply.github.com>
2021-11-03 17:15:52 +0100

    display popular badge due to ratings

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -1,12 +1,20 @@
 import React from 'react';
 
 function Movie(props) {
+
+  // let popularMovie;
+  // if (props.rating >= 9) {
+  //  popularMovie = <p><strong>Popular</strong></p>;
+   
+  // }
   return (
     <div className="movie">
-    <h2>{props.title} </h2>
-    <p>Year: {props.year}</p>
-    <p>Rating:{props.rating}</p>
-  </div>
+      <h2>{props.title} </h2>
+      <p>Year: {props.year}</p>
+      <p>Rating:{props.rating}</p>
+      {/* {popularMovie} */}
+      {props.rating >=8 ?<h4 className="popular"><strong>Popular</strong></h4> : ""}
+    </div>
   );
 }
 
