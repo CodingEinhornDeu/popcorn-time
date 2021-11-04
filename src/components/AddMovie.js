e706904e128f47ed32fe6616659b36c1350562af
CodingEinhornDeu <88826323+CodingEinhornDeu@users.noreply.github.com>
2021-11-04 12:44:42 +0100

    Add onChange event,update state with the new value

--- src/components/AddMovie.js
+++ src/components/AddMovie.js
@@ -1,15 +1,31 @@
 import React, { Component } from 'react'
 import './AddMovie.css'
+
 class AddMovie extends Component {
 
   constructor(props) {
     super(props);
     this.state = {
-      title: ""
+      title: "",
+      year: "",
+      rating: ""
     };
   }
-  handleTitleChange = (event) =>{
-    this.setState({title:event.target.value});
+
+  // handleTitleChange = (event) =>{
+  //   this.setState({title:event.target.value});
+  // }
+  // handleYearChange = (event) =>{
+  //   this.setState({year:event.target.value});
+  // }
+  // handleRatingChange = (event) =>{
+  //   this.setState({rating:event.target.value});
+  // }
+
+  handleInputChange = (event) => {
+    const nameOfTheInput = event.target.name;
+    this.setState({ [nameOfTheInput]: event.target.value });
+
   }
 
   render() {
@@ -23,7 +39,21 @@ class AddMovie extends Component {
               type="text"
               name="title"
               value={this.state.title}
-              onChange={this.handleTitleChange} />
+              onChange={this.handleInputChange} />
+            Year
+            <input
+              type="number"
+              min="1900"
+              max="2050"
+              name="year"
+              value={this.state.year}
+              onChange={this.handleInputChange} />
+            Rating
+            <input
+              type="text"
+              name="rating"
+              value={this.state.rating}
+              onChange={this.handleInputChange} />
             <button>Submit</button>
           </label>
         </form>
